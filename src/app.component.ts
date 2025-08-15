46ec9f6445f6169067ad68039f0876bc8d5171af
GaudRavi09 <gaudravi09@gmail.com>
2025-08-15 22:13:46 +0530

    style(button): set large size as default for all buttons

--- src/app.component.ts
+++ src/app.component.ts
@@ -4,6 +4,6 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterModule],
-  template: `<router-outlet />>`
+  template: `<router-outlet />`
 })
 export class AppComponent {}
