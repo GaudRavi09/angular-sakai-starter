55e83b41502be2a144cf8fb77ef2e55c4c6c3000
GaudRavi09 <gaudravi09@gmail.com>
2025-08-15 21:45:25 +0530

    style: change invalid placeholder and label colors

--- src/app/components/form/mobile-input/mobile-input.spec.ts
+++ src/app/components/form/mobile-input/mobile-input.spec.ts
@@ -2,20 +2,20 @@ import { MobileInput } from './mobile-input';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 describe('MobileInput', () => {
-    let component: MobileInput;
-    let fixture: ComponentFixture<MobileInput>;
+  let component: MobileInput;
+  let fixture: ComponentFixture<MobileInput>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [MobileInput]
-        }).compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MobileInput]
+    }).compileComponents();
 
-        fixture = TestBed.createComponent(MobileInput);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+    fixture = TestBed.createComponent(MobileInput);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
