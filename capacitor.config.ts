2326f8b80fc33107b1ee95e0cf8f3879ce041504
GaudRavi09 <gaudravi09@gmail.com>
2025-08-17 12:38:32 +0530

    feat(project): add capacitor integration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appName: 'starter',
+  webDir: 'dist/browser',
+  appId: 'dev.ravi.starter'
+};
+
+export default config;
